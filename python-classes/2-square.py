b3b83e98742fe0c16fe37671976ecdc77bffb412
Dierak <diego.b.c.rodriguez@gmail.com>
2024-05-18 23:20:40 +0000

    Testing code

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -9,9 +9,13 @@ class Square:
 
         Args:
             size (int): The size of the new square.
+                        It must be a non-negative integer.
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
