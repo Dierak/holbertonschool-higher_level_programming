88381c1c4d4cde490cf91abd07343aa3805027a9
Dierak <diego.b.c.rodriguez@gmail.com>
2024-05-31 21:52:49 +0000

    Testing

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Defines a text file line-counting function."""
+
+
 def write_file(filename="", text=""):
     """
     Writes a string to a text file (UTF-8) and returns the number of characters written.
